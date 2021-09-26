e7599e1e8f48403b8f3f4e5c5587867c64163e89
Marcelo Melo <marcelomelo.ciandt@sulamerica.com.br>
2021-09-26 18:01:07 -0300

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | Bait News</title>
+        <h1>Hello Next.js</h1>
+      </Head>
+    </>
+  );
+}
