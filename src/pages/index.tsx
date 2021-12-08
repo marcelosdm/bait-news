91767da47083fedbfa3eb6c8a18cbb29d4c6bb5c
Marcelo Melo <marcelo.melo.ter@estacio.br>
2021-12-08 14:32:12 -0300

    feat: :sparkles: Add Home Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,25 @@
 import Head from "next/head";
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Início | Bait News</title>
-        <h1>Hello Next.js</h1>
+        <title>Home | Bait News</title>
       </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>Hey, welcome</span>
+          <h1>
+            News about the <span>Software Engineering</span> world.
+          </h1>
+          <p>
+            Get access to all the publications and articles <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   );
 }
